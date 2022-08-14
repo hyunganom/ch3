5a4a1ab28fda46398790c9c0c731354a5f623c2e
hyunganom <dlguswo0504@naver.com>
2022-08-14 17:30:16 +0900

    흠

--- src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -3,38 +3,33 @@ package com.fastcampus.ch3.diCopy1;
 import java.io.FileReader;
 import java.util.Properties;
 
-class Car{}
+class Car {}
 class SportsCar extends Car{}
-class Truck extends Car{}
+class Truck extends Car {}
 class Engine {}
-
 
 public class Main1 {
     public static void main(String[] args) throws Exception {
-        Car car = (Car) getObject("car");
-        Engine engine = (Engine)getObject("engine");
+        Car car = (Car)getObject("car");
+        Engine engine = (Engine) getObject("engine");
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
     }
-
-   static Car getCar() throws Exception{
-       Properties p =new Properties();
-       p.load(new FileReader("config.txt"));
-
-       Class clazz = Class.forName(p.getProperty("car"));
-
-       return (Car)clazz.newInstance();
-
-    }
-
-    static Object getObject(String key) throws Exception{
-        Properties p =new Properties();
+    static Object getObject(String key) throws Exception {
+        Properties p = new Properties();
         p.load(new FileReader("config.txt"));
 
         Class clazz = Class.forName(p.getProperty(key));
 
-        return (Car)clazz.newInstance();
-
+        return clazz.newInstance();
     }
 
+    static Car getCar() throws Exception {
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty("car"));
+
+        return (Car)clazz.newInstance();
+    }
 }
--- src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -6,37 +6,34 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-class Car{}
+class Car {}
 class SportsCar extends Car{}
-class Truck extends Car{}
+class Truck extends Car {}
 class Engine {}
 
-class AppContext{
-    Map map; // 객체 저장소
+class AppContext {
+    Map map ; // 객체 저장소
 
-    AppContext(){
-        map = new HashMap();
-
+    AppContext() {
         try {
-            Properties p =new Properties();
+            Properties p = new Properties();
             p.load(new FileReader("config.txt"));
 
-            //properties에 저장된 내용을 map에 저장
+            // Properties에 저장된 내용을 Map에 저장
             map = new HashMap(p);
 
-            //반복문으로 클래스 이름을 얻어서 객체를 생성해서 다시 map에 저장
+            // 반복문으로 클래스 이름을 얻어서 객체를 생성해서 다시 map에 저장
             for(Object key : map.keySet()){
-                Class clazz = Class.forName((String) map.get(key));
+                Class clazz = Class.forName((String)map.get(key));
                 map.put(key, clazz.newInstance());
-
             }
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
-    Object getBeen(String key){
+    Object getBean(String key) {
         return map.get(key);
     }
 }
@@ -44,13 +41,9 @@ class AppContext{
 public class Main2 {
     public static void main(String[] args) throws Exception {
         AppContext ac = new AppContext();
-
-        Car car = (Car) ac.getBeen("car");
-        Engine engine = (Engine)ac.getBeen("engine");
+        Car car = (Car) ac.getBean("car");
+        Engine engine = (Engine) ac.getBean("engine");
         System.out.println("car = " + car);
         System.out.println("engine = " + engine);
     }
-
-
-
 }
